fa8aa736e4eab045501bde64b261921676a03946
fomy <fumin@hust.edu.cn>
2015-08-13 09:10:36 +0800

    mod -> &

--- simulation.py
+++ simulation.py
@@ -59,7 +59,7 @@ class Simulation:
     def run_iterations(self, iterations):
         for self.cur_i in xrange(iterations):
 
-            if self.cur_i % 15000 == 0:
+            if (self.cur_i & 16383) == 0:
                 progress = 1.0*self.cur_i/self.iterations
                 num = int(progress * 100)
                 print >> sys.stderr,  "%6.2f%%: [" % (progress*100), "\b= "*num, "\b\b>", " "*(100-num), "\b\b]", "%3dd%2dh%2dm%2ds \r"% self.get_runtime(),
